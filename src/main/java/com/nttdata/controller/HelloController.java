5fc7caac39e533b6d77166a4c8c25df63a008585
Zoltan93 <zoltan1993@gmail.com>
2025-02-14 13:40:50 +0100

    Add tmpfs endpoint

--- src/main/java/com/nttdata/controller/HelloController.java
+++ src/main/java/com/nttdata/controller/HelloController.java
@@ -8,8 +8,10 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
+@Path("/")
 @Slf4j
 public class HelloController {
 
@@ -19,13 +21,18 @@ public class HelloController {
         return Response.ok().entity("Hello World!").build();
     }
 
-    //docker run --name practice-docker -p 8080:8080 -v C:\Users\Zoltan\Desktop\practice_volume.txt:/practice_volume.txt practice-docker:1.0.0-SNAPSHOT
+    //docker run --name practice-docker -p 8080:8080 --mount type=volume,src=practice,dst=/practice practice-docker:1.0.0-SNAPSHOT
+    // NOTE: This demonstrates, that whenever we create a volume, add it to the container and then write to that volume (dir),
+    // the volume will preserve it's state even after the container has been deleted
     @Path("/volume")
     @GET
     public Response volumeEndpoint() {
         log.atInfo().log("volumeEndpoint has been called");
         try {
-            return Response.ok().entity(FileUtils.readFileToString(new File("practice_volume.txt"), StandardCharsets.UTF_8)).build();
+            PrintWriter writer = new PrintWriter("practice/practice_volume.txt", StandardCharsets.UTF_8);
+            writer.println("Volume");
+            writer.close();
+            return Response.ok().entity(FileUtils.readFileToString(new File("practice/practice_volume.txt"), StandardCharsets.UTF_8)).build();
         } catch (IOException e) {
             throw new RuntimeException("File is not existent", e);
         }
@@ -39,6 +46,22 @@ public class HelloController {
         log.atInfo().log("bindEndpoint has been called");
         try {
             return Response.ok().entity(FileUtils.readFileToString(new File("practice_bind.txt"), StandardCharsets.UTF_8)).build();
+        } catch (IOException e) {
+            throw new RuntimeException("File is not existent", e);
+        }
+    }
+
+    //docker run --name practice-docker -p 8080:8080 --mount type=tmpfs,dst=/practice practice-docker:1.0.0-SNAPSHOT
+    // NOTE: The volume will only be persistent in the container. Once the container is deleted, the volume disappears with it
+    @Path("/tmpfs")
+    @GET
+    public Response tmpfsEndpoint() {
+        log.atInfo().log("tmpfsEndpoint has been called");
+        try {
+            PrintWriter writer = new PrintWriter("practice/practice_tmpfs.txt", StandardCharsets.UTF_8);
+            writer.println("tmpfs");
+            writer.close();
+            return Response.ok().entity(FileUtils.readFileToString(new File("practice/practice_tmpfs.txt"), StandardCharsets.UTF_8)).build();
         } catch (IOException e) {
             throw new RuntimeException("File is not existent", e);
         }
